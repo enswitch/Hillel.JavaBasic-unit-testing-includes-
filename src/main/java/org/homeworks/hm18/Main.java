f3a98df286ccf99c2cc4857834f6f53811708922
Pavlo Parubchak <pavloparubchak@gmail.com>
2023-03-01 16:17:50 +0200

    hm18 update

--- src/main/java/org/homeworks/hm18/Main.java
+++ src/main/java/org/homeworks/hm18/Main.java
@@ -6,25 +6,19 @@ public class Main {
         Androids android = new Androids("Xiaomi", "Redmi Note 12 Pro", 2022, 256, 649.99, false);
         iPhones iphone = new iPhones("14 Plus", "2146868429304", 2022, 512, 999.99, true);
 
-        android.call();
-        android.sms();
-        android.internet();
+
         android.releasedByAssistance();
         android.androidLanguage();
-        System.out.println();
 
-        System.out.println(android.toString());
-
-        System.out.println();
-
-        iphone.call();
-        iphone.sms();
-        iphone.internet();
         iphone.releasedBy();
         iphone.iOSLanguages();
-        System.out.println();
-        System.out.println(iphone.toString());
 
+        Smartphones smartphone [] = {android, iphone};
+        for (Smartphones s: smartphone) {
+            s.call();
+            s.sms();
+            s.internet();
+        }
 
 
     }
